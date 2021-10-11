985233ba6eec931dfa8bf028143405cf8f8ef26c
soonyh <soonyh@126.com>
2021-10-11 20:01:19 +0800

    init

---
+++ src/constants/index.ts
@@ -0,0 +1,73 @@
+import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap-libs/sdk'
+
+export const ROUTER_ADDRESS = '0xCc7aDc94F3D80127849D2b41b6439b7CF1eB4Ae0'
+
+// a list of tokens by chain
+type ChainTokenList = {
+  readonly [chainId in ChainId]: Token[]
+}
+
+export const CAKE = new Token(ChainId.BSCTESTNET, '0xd97cfa4ec46d4f595c4a8004af5fff5817023e11', 18, 'CAKE', 'PancakeSwap Token')
+export const WBNB = new Token(ChainId.BSCTESTNET, '0xE8aB95C4e12afC4abeaDa075B452c8fac03e6eF9', 18, 'WBNB', 'Wrapped BNB')
+export const TT = new Token(ChainId.BSCTESTNET, '0x755a3CE133abEec6740e0F89a4db3D68EE4aF3Be', 18, 'TT', 'TT Token')
+export const SS = new Token(ChainId.BSCTESTNET, '0xD97cfa4EC46D4F595C4A8004Af5FfF5817023e11', 18, 'SS', 'SS BNB')
+
+
+const WETH_ONLY: ChainTokenList = {
+  [ChainId.MAINNET]: [WETH[ChainId.MAINNET]],
+  [ChainId.BSCTESTNET]: [WETH[ChainId.BSCTESTNET]],
+}
+
+// used to construct intermediary pairs for trading
+export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
+  ...WETH_ONLY,
+  [ChainId.BSCTESTNET]: [...WETH_ONLY[ChainId.BSCTESTNET]],
+}
+
+/**
+ * Some tokens can only be swapped via certain pairs, so we override the list of bases that are considered for these
+ * tokens.
+ */
+export const CUSTOM_BASES: { [chainId in ChainId]?: { [tokenAddress: string]: Token[] } } = {
+  [ChainId.BSCTESTNET]: {},
+}
+
+// used for display in the default list when adding liquidity
+export const SUGGESTED_BASES: ChainTokenList = {
+  ...WETH_ONLY,
+  [ChainId.BSCTESTNET]: [...WETH_ONLY[ChainId.BSCTESTNET]],
+}
+
+// used to construct the list of all pairs we consider by default in the frontend
+export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
+  ...WETH_ONLY,
+  [ChainId.BSCTESTNET]: [...WETH_ONLY[ChainId.BSCTESTNET]],
+}
+
+export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
+  [ChainId.BSCTESTNET]: [
+    [TT, SS]
+  ],
+}
+
+export const NetworkContextName = 'NETWORK'
+
+// default allowed slippage, in bips
+export const INITIAL_ALLOWED_SLIPPAGE = 80
+// 20 minutes, denominated in seconds
+export const DEFAULT_DEADLINE_FROM_NOW = 60 * 20
+
+// one basis point
+export const ONE_BIPS = new Percent(JSBI.BigInt(1), JSBI.BigInt(10000))
+export const BIPS_BASE = JSBI.BigInt(10000)
+// used for warning states
+export const ALLOWED_PRICE_IMPACT_LOW: Percent = new Percent(JSBI.BigInt(100), BIPS_BASE) // 1%
+export const ALLOWED_PRICE_IMPACT_MEDIUM: Percent = new Percent(JSBI.BigInt(300), BIPS_BASE) // 3%
+export const ALLOWED_PRICE_IMPACT_HIGH: Percent = new Percent(JSBI.BigInt(500), BIPS_BASE) // 5%
+// if the price slippage exceeds this number, force the user to type 'confirm' to execute
+export const PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN: Percent = new Percent(JSBI.BigInt(1000), BIPS_BASE) // 10%
+// for non expert mode disable swaps above this
+export const BLOCKED_PRICE_IMPACT_NON_EXPERT: Percent = new Percent(JSBI.BigInt(1500), BIPS_BASE) // 15%
+
+// used to ensure the user doesn't send so much ETH so they end up with <.01
+export const MIN_ETH: JSBI = JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(16)) // .01 ETH
